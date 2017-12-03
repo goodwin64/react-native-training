aea738d61861fa29b5a73920b9de4c1acec11c93
Maksym_Donchenko <maksym_donchenko@epam.com>
2017-12-04 00:18:56 +0200

    feat: About screen Header - added prop types, didn't help

--- components/About/About.js
+++ components/About/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
 import {
     Header,
@@ -25,6 +26,14 @@ class BackButton extends Component {
 }
 
 export default class About extends Component {
+    static propTypes = {
+        onBack: PropTypes.func,
+    };
+
+    static defaultProps = {
+        onBack: () => {},
+    };
+
     render() {
         return (
             <View>
