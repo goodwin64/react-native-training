77dcb03868d68ff1a7ad9b62583572824b00562c
Maksym_Donchenko <maksym_donchenko@epam.com>
2017-12-03 21:53:50 +0200

    feat: add header layout and styles

--- components/UserRoom/UserRoom.js
+++ components/UserRoom/UserRoom.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, Button } from 'react-native';
+import { Header } from 'react-native-elements';
 
 import { colors } from '../../styles/constants';
 import UserRoomStyles from './UserRoom.styles';
@@ -8,6 +9,13 @@ export default class extends Component {
     render() {
         return (
             <View style={UserRoomStyles.container}>
+                <Header
+                    leftComponent={{ icon: 'menu', color: '#fff' }}
+                    centerComponent={{ text: 'epamer', style: { color: '#fff' } }}
+                    rightComponent={{ icon: 'home', color: '#fff' }}
+                    outerContainerStyles={UserRoomStyles.header}
+                />
+
                 <Text>
                     Your username: {this.props.username}
                 </Text>
--- components/UserRoom/UserRoom.styles.js
+++ components/UserRoom/UserRoom.styles.js
@@ -4,6 +4,16 @@ import { colors } from '../../styles/constants';
 
 export default StyleSheet.create({
     container: {
-        backgroundColor: colors.LIGHT_GRAY
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: colors.LIGHT_GRAY,
+        justifyContent: 'space-between',
+        alignItems: 'stretch',
+    },
+    header: {
+        flex: 0.05,
+        backgroundColor: '#111',
+        paddingTop: 25,
+        // alignSelf: 'stretch',
     },
 });
