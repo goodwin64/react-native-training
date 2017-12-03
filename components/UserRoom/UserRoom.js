6d4a1f39db3669d459415a7f884f8e4cb9d42edd
Maksym_Donchenko <maksym_donchenko@epam.com>
2017-12-03 23:17:58 +0200

    feat: add <Progress.Bar/> to User Room and logic + styles for it

--- components/UserRoom/UserRoom.js
+++ components/UserRoom/UserRoom.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, Button, Image } from 'react-native';
 import { Header } from 'react-native-elements';
+import * as Progress from 'react-native-progress';
 
 import {
     assets,
@@ -8,7 +9,45 @@ import {
 } from '../../styles/constants';
 import UserRoomStyles from './UserRoom.styles';
 
+const DIFF_ON_TICK = 0.005;
+// const DIFF_ON_TAP = 0.02; // TODO: add logic on tap (increase bar value)
+
 export default class extends Component {
+    constructor() {
+        super();
+        this.state = {
+            progressBars: [
+                { title: 'Personal', value: 0.1 },
+                { title: 'Project Activities', value: 0.5 },
+                { title: 'Soft Skills', value: 0.75 },
+                { title: 'Hard Skills', value: 0.6 },
+            ]
+        };
+        this.timerId = null;
+    }
+
+    componentDidMount() {
+        this.tickProgressBars();
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timerId);
+    }
+
+    tickProgressBars = () => {
+        this.timerId = setTimeout(() => {
+            this.setState({
+                progressBars: this.state.progressBars.map(bar => ({
+                    ...bar,
+                    value: bar.value > DIFF_ON_TICK
+                        ? bar.value - DIFF_ON_TICK
+                        : 0,
+                }))
+            });
+            this.tickProgressBars();
+        }, 100);
+    };
+
     render() {
         return (
             <View style={UserRoomStyles.container}>
@@ -23,6 +62,20 @@ export default class extends Component {
                     style={UserRoomStyles.coderImage}
                     source={assets.IMAGE_CODER}
                 />
+
+                {
+                    this.state.progressBars.map(bar => (
+                        <View key={bar.title}>
+                            <Text>
+                                {bar.title}
+                            </Text>
+                            <Progress.Bar
+                                progress={bar.value}
+                                width={200}
+                            />
+                        </View>
+                    ))
+                }
 
                 <Button
                     onPress={this.props.onLogout}
